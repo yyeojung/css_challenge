4f58755e6db26d26cebb36cabe3d8d807aca4b82
hongyeojung <hongshii0i0i@gmail.com>
2024-03-08 09:43:12 +0900

    240308 배경바꾸기

--- common.js
+++ common.js
@@ -2,13 +2,31 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   menuHandler();
+  changeBg();
 });
 
-  const menuHandler = () => {
+const menuHandler = () => {
     const menuBtn = document.querySelector('.menu-icon');
     menuBtn.addEventListener('click', function() {
-      menuBtn.classList.toggle("active");
-      menuBtn.classList.remove("no_animation")
+        menuBtn.classList.toggle("active");
+        menuBtn.classList.remove("no_animation")
     });
-  };
+};
 
+// 5. 배경바꾸기
+const changeBg = () => {
+    for(let i = 1; i<=3; i++) {
+        const checkBox= document.getElementById(`check-${i}`);
+        checkBox.addEventListener('click', function() {
+            const background = document.querySelector('.frame .bg');
+            console.log(checkBox, background.style.left)
+            if(checkBox.classList.contains('check-1')) {
+                background.style.left = '0px';
+            } else if (checkBox.classList.contains('check-2')) {
+                background.style.left = '-400px';
+            } else {
+                background.style.left = '-800px';
+            }
+        })
+    }
+}
